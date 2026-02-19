9193a941465982b0656efed68d0cca315bd4d157
alexandermcclelland <144083355+alexandermcclelland@users.noreply.github.com>
2026-02-19 13:27:30 +1100

    [MLOPS-5674] Upgrade KFP to 2.5.0 (#9)

--- generator/templates/manifests/kubeflow-tools/poddefaults-webhook/sync_upstream.sh
+++ generator/templates/manifests/kubeflow-tools/poddefaults-webhook/sync_upstream.sh
@@ -7,8 +7,8 @@ cd "$THIS_SCRIPT_PATH"
 
 # upstream configs
 UPSTREAM_REPO="github.com/kubeflow/manifests"
-UPSTREAM_PATH="apps/admission-webhook/upstream/overlays/cert-manager"
-UPSTREAM_REF="14c0f9abe70c4d0ce3e021a5839a7cdd54dc572d" # v1.7.0
+UPSTREAM_PATH="applications/admission-webhook/upstream/overlays/cert-manager"
+UPSTREAM_REF="d8d643f1a736b28c525bd9b8ee5c9b2f4a661b39" # v1.10.2
 
 # output configs
 OUTPUT_PATH="./upstream"
@@ -18,4 +18,4 @@ rm -rf "$OUTPUT_PATH"
 
 # localize the upstream resources with kustomize
 # - https://kubectl.docs.kubernetes.io/references/kustomize/cmd/localize/
-kustomize localize "${UPSTREAM_REPO}/${UPSTREAM_PATH}?ref=${UPSTREAM_REF}" "$OUTPUT_PATH"
+kustomize localize "${UPSTREAM_REPO}/${UPSTREAM_PATH}?ref=${UPSTREAM_REF}" "$OUTPUT_PATH" --no-verify
